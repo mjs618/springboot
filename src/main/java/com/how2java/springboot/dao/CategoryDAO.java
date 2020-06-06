459f6a947ef980e0a64e61668ae161cba0c78479
maojinsong <1024mjs@gmail.com>
2020-06-06 16:56:18 +0800

    使用jpa获取数据

--- src/main/java/com/how2java/springboot/dao/CategoryDAO.java
+++ src/main/java/com/how2java/springboot/dao/CategoryDAO.java
@@ -4,5 +4,5 @@ package com.how2java.springboot.dao;
 import com.how2java.springboot.pojo.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//public interface CategoryDAO extends JpaRepository<Category,Integer> {
-//}
+public interface CategoryDAO extends JpaRepository<Category,Integer> {
+}
